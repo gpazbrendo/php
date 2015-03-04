7e5cbf9796dd3bdc31b641d86b50ccdf711825cd
Gilvan Paz <gpazbrendo@gmail.com>
2015-03-04 12:24:29 -0300

    Operadores aritméticos

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>Testando PHP</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h2>Curso de PHP Basico</h2>
+	<a href="operadoresaritimetricos.php">Aritmética</a>
+	
+</body>
+</html>
